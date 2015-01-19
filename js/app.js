cf995b43f5936d7213b4710c3b012b197c113211
Jordan Johnson <robertjohnson@Roberts-MacBook-Air-2.local>
2015-01-19 10:01:11 -0600

    Moved all JS externally, added favicon

--- js/app.js
+++ js/app.js
@@ -5,84 +5,79 @@ $(document).ready(function(){
   $("#movie_box").hide();
 });
 
+$("#loader").hide();
 
-/* GET HITTING ENTER TO FIRE CLICK SUBMIT */
-/*
-$( "#submit_search-button" ).click(function() {
-    var searchAni = new TimelineLite();
-    searchAni.to( $("#home-text"), 1, {top: -200} );
+    var source   = $("#movie_script").html();
+    var template = Handlebars.compile(source);
 
-    $("#movie-list").html(''); 
-    $("#movie_box").show("slow");
+    var searchSource   = $("#results_script").html();
+    var searchTemplate = Handlebars.compile(searchSource);
 
-    var searchTitle = $("#home_movie_input").val();
+    var router = new Grapnel({ pushState: false });
 
-    $.getJSON( "http://www.omdbapi.com/?t='" + searchTitle + "'&y=&plot=short&r=json", function( data ) {
-      var items = [];
-      $.each( data, function( key, value ) {
-        items.push( "<li class='movie_" + key + "'>" + value + "</li>" );
-      });
+    router.get('/search/:title?', function(req){
+        
+        var title = req.params.title;
+        $("#home_movie_input").val(title);
 
-      $( "<ul/>", {
-        "class": "my-new-list",
-        "id" : "movie-list",
-        html: items.join( "" )
-      }).appendTo( ".movie_info-container" );
+        var searchAni = new TimelineLite({onComplete:movieSearch});
+        searchAni.to( $(".color_overlay"), 1, {backgroundColor: 'rgb(255, 193, 7)'});
+        searchAni.to( $("#home-text"), 1, {top: -270});
+        searchAni.to( $("#movie_box"), 1, {top: -270});
+        
+        $( "#movie_details-row" ).html("");
+        function movieSearch(){
+          $("#loader").show();
+
+          $.getJSON( "http://www.omdbapi.com/?s='" + title + "'&r=json&type=movie", function( data ) {
+            var html = searchTemplate(data);
+            //console.log(data);
+            //console.log(html);
+            $("#movie_results-row").html(html);
+            var movieListPlaced = new TimelineLite();
+              movieListPlaced.fromTo("#search-results", 1, { autoAlpha:0, ease:Linear.easeNone}, {autoAlpha:1, ease:Linear.easeNone});
+            $("#loader").hide();
+          });
+        }
     });
-});
-*/
-/*
 
-!function ($) {
-    $(function () {
-        $('#t').keyup(function () {
-            if (event.keyCode == 13) {
-                $('#search-by-title-button').click();
-            } else {
-                return false;
-            }
-        });
-        $('#i').keyup(function () {
-            if (event.keyCode == 13) {
-                $('#search-by-id-button').click();
-            } else {
-                return false;
-            }
-        });
-        $('#search-by-title-button').click(function () {
-            var c = $('#home_movie_input').val();
-            var d = 'http://www.omdbapi.com/?' + c;
-            var e = $('#search-by-title-request');
-            e.find('a').attr('href', d).html(d);
-            e.show('slow');
-            var f = $('#search-by-title-response');
-            $.ajax({
-                type: 'GET',
-                dataType: 'text',
-                url: '/?' + c,
-                statusCode: {
-                    403: function () {
-                        f.find('pre').html('HTTP 403 Forbidden!');
-                    }
-                },
-                success: function (a) {
-                    f.find('pre').html(a.replace(/</g, '&lt;').replace(/>/g, '&gt;'));
-                },
-                complete: function () {
-                    f.show('slow');
-                }
-            });
-        });
-        $('#search-by-title-reset').click(function () {
-            var a = $('#search-by-title-request');
-            a.hide('slow');
-            a.find('a').attr('href', 'javascript:;').html('');
-            var b = $('#search-by-title-progress');
-            b.hide('slow');
-            var c = $('#search-by-title-response');
-            c.hide('slow');
-            c.find('pre').html('');
-        });
+    router.get('/movie/:id?', function(req){
+        var id = req.params.id;
+        $(".color_overlay").css("background-color", "rgb(255, 193, 7)");
+        $("#home-text").css("top", "-270px");
+        $("#movie_box").css("top", "-270px");
+        $("#movie_box").css("opacity", "1");
+
+        function resultsSlide(){
+          $("#loader").show();
+          
+          var listAni = new TimelineLite({onComplete:moviePlace});
+          listAni.to( $("#search-results"), 1.5, {top: 350});
+          $("html, body").animate({ scrollTop: 0 }, "slow");
+          return false;
+        }
+        resultsSlide();
+        function moviePlace(){
+          $.getJSON( "http://www.omdbapi.com/?i="+ id + "&r=json&type=movie&plot=short", function( data ) {
+            var html = template(data);
+            //console.log(data);
+            $( "#movie_details-row" ).html(html);
+              var moviePlaced = new TimelineLite();
+              moviePlaced.fromTo("#movie_box", 1, { autoAlpha:0, ease:Linear.easeNone}, {autoAlpha:1, ease:Linear.easeNone});
+              moviePlaced.to( $("#search-results"), 1, {top: -200});
+
+              $("#loader").hide();
+
+
+
+          });
+        }
     });
-} (window.jQuery);
-*/
+
+
+    $( "#submit_search-button" ).click(function(e) {
+        var searchTitle = $("#home_movie_input").val();
+        router.navigate('/search/' + searchTitle);
+        $("#loader").show();
+
+    });
